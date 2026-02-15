fab13d592635135b9187d097bfe174509fc1adc8
Kanish Dhiman <dhimankanish02@gmail.com>
2026-02-16 01:19:05 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hummingbird-kanish",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scikit-learn"
+    ],
+    author="Kanishth Dhiman",
+    description="AHO algorithm for feature selection in machine learning pipelines.",
+    url="https://github.com/KanishDhiman02/hummingbird-kanish",
+)
